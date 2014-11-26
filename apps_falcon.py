0d556d271e3bf88b76173bc8100ae81b978397c9
ali kusnadi <kugutsu.hiruko@gmail.com>
2014-11-26 23:40:16 +0700

    add index respone

--- apps_falcon.py
+++ apps_falcon.py
@@ -1,9 +1,15 @@
 import falcon
+import json
 
 class TestResource(object):
     def on_get(self, request, response):
         json_data = {"message": "Hello, World!"}
         response.body = json.dumps(json_data)  
 
+class IndexResource(object):
+    def on_get(self, request, response):
+        response.body = 'Hello World!'
+
 app = falcon.API()
+app.add_route('/', IndexResource())
 app.add_route('/json', TestResource())
